cdfe760e5a526350427c7f66cc8926747f7a0017
Austin Fleming <austin@flimflamfactory.com>
2023-09-28 22:20:39 -0700

    chore: clean up repo

---
+++ huffathon_ctf_1/script/Deploy.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "foundry-huff/HuffDeployer.sol";
+import "forge-std/Script.sol";
+
+interface SimpleStore {
+    function setValue(uint256) external;
+
+    function getValue() external returns (uint256);
+}
+
+contract Deploy is Script {
+    function run() public returns (SimpleStore simpleStore) {
+        simpleStore = SimpleStore(HuffDeployer.deploy("MyContract"));
+    }
+}
